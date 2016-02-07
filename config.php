88e575c8d6127fd9133e8b8f90394e50c48ae353
coinableS <coinables@gmail.com>
2016-02-06 22:01:23 -0800

    buy sell

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+$api_key = "JQmCzDlci9ehliKd1pZ8gXRKqxBKoyEq8PoLocXOu12";
+$api_secret = "H2IdizuS0Erilu0xfwuxicdybXTTMbAkrRpTcVOz6C9";
+
+?>
--- finex.php
+++ finex.php
@@ -30,21 +30,24 @@ class bitfinex{
    {
       $request = "/v1/order/cancel";
 	  $data = array(
+	     "request" => $request,
 	     "order_id" => $order_id
 	  );
 	  return $this->hash_request($data);
    }
    
-   //kill switch, cancel all orders
    public function kill_switch()
    {
    $request = "/v1/order/cancel/all";
+   $data = array(
+      "request" => $request
+   );
    return $this->hash_request($data);
    }
 
    private function headers($data)
    {
-      $data["nonce"] = strval(round(microtime(true),0));
+      $data["nonce"] = strval(round(microtime(true) * 10,0));
       $payload = base64_encode(json_encode($data));
       $signature = hash_hmac("sha384", $payload, $this->secret);
       return array(
@@ -64,7 +67,7 @@ class bitfinex{
          CURLOPT_POST => true,
          CURLOPT_RETURNTRANSFER => true,
          CURLOPT_HTTPHEADER => $headers,
-         CURLOPT_SSL_VERIFYPEER => true,
+         CURLOPT_SSL_VERIFYPEER => false,
          CURLOPT_POSTFIELDS => ""
       ));
       $ccc = curl_exec($ch);
@@ -73,9 +76,38 @@ class bitfinex{
 
 }
 
-$trade = new bitfinex("JQmCzDlci9ehliKd1pZ8gXRKqxBKoyEq8PoLocXOu12", "H2IdizuS0Erilu0xfwuxicdybXTTMbAkrRpTcVOz6C9");
-$margin_limit_buy = $trade->new_order("BTCUSD", "0.01", "375.85", "buy", "limit");
-echo $margin_limit_buy['message'];
+require_once('config.php');
+
+$getask = json_decode(file_get_contents("https://api.bitfinex.com/v1/pubticker/BTCUSD"), true);
+$ask = $getask['ask'];
+
+$getbid = json_decode(file_get_contents("https://api.bitfinex.com/v1/pubticker/BTCUSD"), true);
+$bid = $getbid['bid'];
+
+$getavg = json_decode(file_get_contents("https://api.bitcoinaverage.com/ticker/global/USD/"), true);
+$avg = $getavg['24h_avg'];
+
+$diff = $avg - $ask;
+$diff2 = $bid -  $avg;
+
+if($diff > 2){
+//good time to buy
+echo "24h Avg: ".$avg."<br>";
+echo "Current Ask: ".$ask."<br>";
+$trade = new bitfinex($api_key, $api_secret);
+$buy = $trade->new_order("BTCUSD", "0.01", "1", "buy", "exchange market");
+print_r($buy);
+}
+
+if($diff2 > 2){
+//good time to sell
+echo "24h Avg: ".$avg."<br>";
+echo "Current Bid: ".$bid."<br>";
+$trade = new bitfinex($api_key, $api_secret);
+$sell = $trade->new_order("BTCUSD", "0.01", "1", "sell", "exchange market");
+print_r($sell);
+}
+
 
 ?>
 
